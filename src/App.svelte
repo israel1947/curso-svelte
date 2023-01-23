e19d7720eb0e7aaee0b05947fbb7f76c08f15cb9
israel_1947 <israeldavidsdvsf@gmail.com>
2023-01-23 14:03:41 +0100

    creando e implementando componentes

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+	import Abaut from "./components/Abaut.svelte";
+
+	export let name;
+	export let apellido;
+</script>
+
+<main>
+	<h1>Hello {name} {apellido}!</h1>
+	<Abaut></Abaut>
+</main>
+
+<style>
+	:global(:root) {
+		--colo-prueba: #880488;
+	}
+
+	p{
+		color: var(--colo-prueba);
+	}
+
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/components/Abaut.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { text } from "svelte/internal";
+
+
+  let texto = `Frontend Developer`
+</script>
+
+<style>
+  p{
+    color: var(--colo-prueba);
+  }
+</style>
+
+<div class="About">
+  <p>{texto}</p>
+</div>
